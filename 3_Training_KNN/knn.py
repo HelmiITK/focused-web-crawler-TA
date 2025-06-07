8a6017a5ba6244c1ef7dd3565a6079db3d47fbd3
Helmi <helmi220602@gmail.com>
2025-06-08 00:46:46 +0800

    BISMILLAH FINAL

--- 3_Training_KNN/knn.py
+++ 3_Training_KNN/knn.py
@@ -13,7 +13,7 @@ y = pd.read_csv('D:\\Semester 8\TA\\focused-web-crawler\\2_Preprocessing\\embedd
 
 # Split dan train
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
-knn = KNeighborsClassifier(n_neighbors=4, metric='cosine')
+knn = KNeighborsClassifier(n_neighbors=14, metric='cosine')
 # knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 
